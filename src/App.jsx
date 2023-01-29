9123ed0b0c1801b09bd050ae087780f275c695d1
Palvaneh <Palvaneh@Ideal.local>
2023-01-29 17:21:38 +0330

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
+
+const App = () => {
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({})
+
+  const search = async (e)=>{
+    if(e.key === "Enter"){
+      const data = await fetchWeather(query)
+      setWeather(data)
+      setQuery("")
+    }
+  }
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {
+        weather.main && (
+          <div className=""></div>
+        )
+      }
+    </div>
+  );
+};
+
+export default App;

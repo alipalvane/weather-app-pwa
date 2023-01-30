47c2bab578eeb5d772c05400ca9fea1272178417
Palvaneh <Palvaneh@Ideal.local>
2023-01-30 09:18:09 +0330

    change background image dir and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import { fetchWeather } from "./api/fetchWeather";
 import "./App.css";
 
 const App = () => {
-
   const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState({});
 
-  const search = async (e)=>{
-    if(e.key === "Enter"){
-      const data = await fetchWeather(query)
-      setWeather(data)
-      setQuery("")
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
     }
-  }
+  };
 
   return (
     <div className="main-container">
@@ -25,11 +24,26 @@ const App = () => {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
-      {
-        weather.main && (
-          <div className=""></div>
-        )
-      }
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+              className="city-icon"
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
